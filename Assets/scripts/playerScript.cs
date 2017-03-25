a55302b771743f2a681ae2be923435dae44946f0
jbonto <jrbonto1002@gmail.com>
2017-03-24 21:11:17 -0400

    Add files via upload

---
+++ Assets/scripts/playerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class playerScript : MonoBehaviour {
+	public float speed, HP;
+	public Image hpBar;
+	private Rigidbody2D RB2D;
+	// Use this for initialization
+	void Start () {
+		RB2D = GetComponent<Rigidbody2D> ();
+		hpBar.transform.localScale = new Vector2 (HP, hpBar.transform.localScale.y);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RB2D.velocity = new Vector2 (speed, RB2D.velocity.y);
+	}
+	public void recieveDamage(){
+		HP -= (Time.deltaTime*2);
+		hpBar.transform.localScale = new Vector2 (HP, hpBar.transform.localScale.y);
+	}
+}
---
+++ Assets/scripts/spikeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D(Collider2D col){
+		col.GetComponent<playerScript> ().recieveDamage ();
+	}
+}
